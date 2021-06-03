9aabb596a36c65a831f23082c470c0d6746d9a82
richardcostarocha <83733856+richardcostarocha@users.noreply.github.com>
2021-06-02 21:48:51 -0300

    alteração matriz

--- criar_matriz.py
+++ criar_matriz.py
@@ -1,7 +1,6 @@
 n_linhas = int(input('quantas linhas tem a matriz: '))# recebe numero de linhas
 n_colunas = int(input('quantas colunas tem a matriz: '))# recebe o numero de colunas 
 valor = int(input('qual o valor a ser preenchido: '))
-
 def crie_matriz(n_linhas, n_colunas, valor):
     matriz = list()
     for i in range(n_linhas):
@@ -9,11 +8,9 @@ def crie_matriz(n_linhas, n_colunas, valor):
         for j in range(n_colunas):
             linha.append(valor)
         matriz.append(linha)
-
     return matriz
-
 a = crie_matriz(n_linhas, n_colunas, valor)
 for l in range(n_linhas):
     for c in range(n_colunas):
         print(f'[{a[l][c]}]', end='')
-    print()
+    print('')
